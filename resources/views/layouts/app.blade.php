efa7fcba4ca0945af959e133ed34563c453860e0
pmcubom65 <pmcubom@zion.es>
2020-03-27 19:00:29 +0100

    hecha la barra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
                 </div>
 
 
-        <nav class="navbar navbar-expand-md bg-dark sticky-top" >
+        <nav class="navbar navbar-expand-md sticky-top" >
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -54,19 +54,55 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav" style="Text-color: white;">
             <li class="nav-item">
-                <a class="nav-link" href="#">link1</a>
+                <a class="nav-link" href="#">Información</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">link1</a>
+                <a class="nav-link" href="#">Buscar Trabajo</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">link1</a>
+                <a class="nav-link" href="#">Buscar Cursos de Formación</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">link1</a>
             </li>
         </ul>
         </div>
+
+<!-- Right Side Of Navbar -->
+<ul class="navbar-nav ml-auto ">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item ">
+                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item  dropdown">
+                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+     </ul>
+                
+
+
         </nav>
 
         <div class="container-fluid w-100 px-0">
