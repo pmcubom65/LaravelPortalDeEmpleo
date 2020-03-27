1b3bd2d385f175ddd97fd2403b3ce34303a3b1e8
pmcubom65 <pmcubom@zion.es>
2020-03-27 17:58:10 +0100

    la pantalla inicial ahora tiene buena pinta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,64 +23,44 @@
 <body>
 
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm ">
-            <div class="container">
-                <a class="navbar-brand text-white" href="{{ url('/') }}">
-                    ETT Publica
-                </a>
-              
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                <div class="jumbotron jumbotron-fluid text-white"  >
 
-                    </ul>
-                    
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto ">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item ">
-                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item  ">
-                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-3">
+                                    <a class="navbar-brand" href="{{ url('/') }}"><img src="img/logo.png" class="w-50 h-50"/></a>
                                 </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-           
-           
-        </nav>
-    <!--    <div class="jumbotron jumbotron-fluid bg-primary text-white"  >
+                                <div class="col-lg-9  d-none d-lg-block">
+                                    <h2 class="display-4 text-left">Conectamos trabajadores y empresas</h2>
+                                    <p>Hacemos el trabajo</p>
+                                </div>
 
-                <div class="container">
-                <h2 class="display-2 text-center">ETT Pública</h1>
-                
+
+                            </div>
+
+                        </div>
                 </div>
-        </div>-->
+
+
+        <nav class="navbar navbar-expand-md bg-dark sticky-top" >
+  
+        <ul class="navbar-nav" style="Text-color: white;">
+            <li class="nav-item">
+                <a class="nav-link" href="#">link1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">link1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">link1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">link1</a>
+            </li>
+        </ul>
+        
+        </nav>
 
         <div class="container-fluid w-100 px-0">
         <div class="row w-100 justify-content-center align-items-center mx-0">
