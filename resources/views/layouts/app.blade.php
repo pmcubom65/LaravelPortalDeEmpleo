11271a5b0d592698f63e887aa58fc0a83adee1fc
pmcubom65 <pmcubom@zion.es>
2020-03-28 18:07:21 +0100

    ahora si que tiene estilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +8,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
 
     <title>ETT Publica</title>
     <meta name="description" content="página que sirve como portal de empleo">
@@ -18,116 +19,123 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
 
     <div id="app">
 
-                <div class="jumbotron jumbotron-fluid text-white"  >
+        <div class="jumbotron jumbotron-fluid text-white">
 
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-xs-3">
-                                    <a class="navbar-brand d-none d-lg-block" href="{{ url('/') }}"><img src="img/logo.png" class="w-50 h-50"/></a>
-                                    <a class="navbar-brand d-lg-none" href="{{ url('/') }}"><img src="img/icon.png" class="w-50 h-50"/></a>
-                                </div>
-                                <div class="col-lg-9  d-none d-lg-block">
-                                    <h2 class="display-4 text-left">Conectamos trabajadores y empresas</h2>
-                                    <p>Hacemos el trabajo</p>
-                                </div>
-                                <div class="d-lg-none my-1">
-                                <p text-center>Hacemos trabajo</p>
-                                </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-xs-3">
+                        <a class="navbar-brand d-none d-lg-block" href="{{ url('/') }}"><img src="img/logo.png"
+                                class="w-50 h-50" /></a>
+                        <a class="navbar-brand d-lg-none" href="{{ url('/') }}"><img src="img/icon.png"
+                                class="w-50 h-50" /></a>
+                    </div>
+                    <div class="col-lg-9  d-none d-lg-block">
+                        <h2 class="display-4 text-left">Conectamos trabajadores y empresas</h2>
+                        <p>Hacemos el trabajo</p>
+                    </div>
+                    <div class="d-lg-none my-1">
+                        <p text-center>Hacemos trabajo</p>
+                    </div>
 
 
-                            </div>
-
-                        </div>
                 </div>
 
+            </div>
+        </div>
 
-        <nav class="navbar navbar-expand-md sticky-top" >
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <nav class="navbar navbar-expand-md sticky-top">
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link " href="#informacion">Información</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Buscar Trabajo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Buscar Cursos de Formación</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toogle " aria-expanded="false" data-toggle="dropdown" data-target="dt" href="#">
-                    Categorias Profesionales
-                <span class="caret"></span>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dt">
-                <a class="dropdown-item">Enlace</a>
-</div>
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                        <a class="nav-link " href="#inicio" active>Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#informacion">Información</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Buscar Trabajo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Buscar Cursos de Formación</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toogle " aria-expanded="false" data-toggle="dropdown"
+                            data-target="dt" href="#">
+                            Categorias Profesionales
+                            <span class="caret"></span>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dt">
+                            <a class="dropdown-item">Enlace</a>
+                        </div>
 
-            </li>
-        </ul>
-        </div>
+                    </li>
+                </ul>
+            </div>
 
-<!-- Right Side Of Navbar -->
-<ul class="navbar-nav ml-auto ">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item ">
-                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item  dropdown">
-                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto ">
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item ">
+                    <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item  dropdown">
+                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            {{ __('Logout') }}
+                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-     </ul>
-                
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+
 
 
         </nav>
 
         <div class="container-fluid w-100 px-0">
-        <div class="d-lg-none my-1">
-                                <img class="w-110 h-110" src="https://i.picsum.photos/id/1078/300/300.jpg" />
-        </div>
+          
 
 
-        <div class="row w-100 justify-content-center align-items-center mx-0">
-            @yield('content')
+            <div class="row w-100 justify-content-center align-items-center mx-0">
+                @yield('content')
             </div>
         </div>
     </div>
 
-    
+
 
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,123 +7,127 @@
 
 
 
-        <div class="container-fluid position-ref full-height" >
-  
-        <div class="jumbotron jumbotron-fluid d-none d-lg-block" style="height: 100vh; padding-top: 0;">
-            <div class="container-fluid justify-content-center align-items-center">
-            
+<div class="container-fluid position-ref full-height">
+
+    <div class="container-fluid d-none d-lg-block" style="height: 100vh; padding-top: 0;">
+        <div class="container-fluid justify-content-center align-items-center">
+
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="img-responsive center-block w-110" src="img/dia450.jpg" alt="First slide">
-      <div class="carousel-caption d-top d-md-block">
-    <h1 class="display-2 text-center font-weight-bold">Tu trabajo está ahí fuera</h1>
-    
-  </div>
-    </div>
-    <div class="carousel-item">
-      <img class="img-responsive center-block w-110" src="img/noche450.jpg" alt="Second slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h1 class="display-2 text-center font-weight-bold">Para cumplir tus sueños</h5>
-    <p></p>
-  </div>
-    </div>
-    <div class="carousel-item">
-      <img class="img-responsive center-block w-110" src="img/puente450.jpg" alt="Third slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h1 class="display-2 text-center font-weight-bold">Unimos trabajo</h1>
-    <p></p>
-  </div>
-    </div>
-    
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  </div>
-</div>
-          <section id="intro" class="intro-section">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <h1>Scrolling Nav</h1>
-                            <p>Lorem ipsum</p>
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="img-responsive center-block w-110" src="img/ciudad.jpg" alt="First slide">
+                        <div class="carousel-caption d-top d-md-block">
+                            <h1 class="display-2 text-center font-weight-bold">Tu trabajo está ahí fuera</h1>
+
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-responsive center-block w-110" src="img/noche450.jpg" alt="Second slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="display-2 text-center font-weight-bold">Para cumplir tus sueños</h5>
+                                <p></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-responsive center-block w-110" src="img/tren.jpg" alt="Third slide">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h1 class="display-2 text-center font-weight-bold">Unimos trabajo</h1>
+                            <p></p>
                         </div>
                     </div>
 
                 </div>
-          </section>
-
-
-
-          <section id="informacion" class="informacion-section">
-              <div class="container">
-                    <div class="row">
-                    <div class="col-lg-12">
-                    <div class="row">
-                <div class="col  float-left">
-                <div class="card float-right" style="width: 18rem;">
-                 <img src="https://i.picsum.photos/id/1035/300/300.jpg" class="card-img-top" alt="...">
-                 
-                <div class="card-body">
-                <h5 class="card-title">Soy un trabajador</h5>
-                <p class="card-text">Inscribase en las ofertas de empleo. Actualize su perfil y curriculum. Consulte en el estado de sus candidaturas</p>
-                
-                @if (Route::has('register'))
-               
-                <a href="/login" class="btn btn-primary" id="miboton">Acceder</a>
-               
-                @endif
-
-
-
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            </div>
-                </div>
-               
-                 <div class="col  float-right">
-                 <div class="card float-left" style="width: 18rem;">
-            <img src="https://i.picsum.photos/id/1078/300/300.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Soy una empresa</h5>
-            <p class="card-text">Añada nuevas ofertas de trabajo y actualice las ofertas. Revise las candidaturas. Contacte con los candidatos</p>
-            @if (Route::has('register'))
-            <a href="/login2" class="btn btn-primary">Acceder</a>
-            @endif
-            
-            </div>
-            </div>
-                  </div>
-              </div>
-                    </div>
-                    </div>
-              </div>
-
-
-          </section>
-
-
-
-
-
-
-
-            
-          
-
         </div>
+        <section id="inicio" class="intro-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Scrolling Nav</h1>
+                        <p>Lorem ipsum</p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+
+
+        <section id="informacion" class="informacion-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col  float-left">
+                                <div class="card float-right" style="width: 18rem;">
+                                    <img src="https://i.picsum.photos/id/1035/300/300.jpg" class="card-img-top"
+                                        alt="...">
+
+                                    <div class="card-body">
+                                        <h5 class="card-title">Soy un trabajador</h5>
+                                        <p class="card-text">Inscribase en las ofertas de empleo. Actualize su perfil y
+                                            curriculum. Consulte en el estado de sus candidaturas</p>
+
+                                        @if (Route::has('register'))
+
+                                        <a href="/login" class="btn btn-primary hvr-pulse" id="miboton">Acceder</a>
+
+                                        @endif
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col  float-right">
+                                <div class="card float-left" style="width: 18rem;">
+                                    <img src="https://i.picsum.photos/id/1078/300/300.jpg" class="card-img-top"
+                                        alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Soy una empresa</h5>
+                                        <p class="card-text">Añada nuevas ofertas de trabajo y actualice las ofertas.
+                                            Revise las candidaturas. Contacte con los candidatos</p>
+                                        @if (Route::has('register'))
+                                        <a href="/login2" class="btn btn-primary">Acceder</a>
+                                        @endif
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </section>
+
+
+
+
+
+
+
+
+
 
     </div>
+
+</div>
 
 
 
