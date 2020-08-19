7402468cb365f7437372ad36e1978e486981a73b
pmcubom65 <pmcubom@zion.es>
2020-08-19 16:59:21 +0200

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,8 +52,5 @@ $app->singleton(
 |
 */
 
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
-header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 return $app;
