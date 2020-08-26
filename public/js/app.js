1b145ba4cf111bb157c85022b9dd268d4f34fdaa
pmcubom65 <pmcubom@zion.es>
2020-08-26 16:47:33 +0200

    cloudinary

--- public/js/app.js
+++ public/js/app.js
@@ -2672,7 +2672,7 @@ __webpack_require__.r(__webpack_exports__);
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'POST',
           'Content-Type': 'application/x-www-form-urlencoded',
-          'Access-Control-Allow-Headers': 'accept, Origin, Authorization, Content-Type, X-Auth-Token, Access-Control-Request-Headers, X-Requested-With, Access-Control-Request-Method'
+          'Access-Control-Allow-Headers': '*, accept, Origin, Authorization, Content-Type, X-Auth-Token, Access-Control-Request-Headers, X-Requested-With, Access-Control-Request-Method'
         },
         data: formDatafile
       }).then(function (res) {
