8c65ef0645baa56ad3b88af6e7c088c1b7a4c2f2
pmcubom65 <pmcubom@zion.es>
2020-08-26 16:12:24 +0200

    cloudinary

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', "http://pedro-manuel-cubo-laravelvue.herokuapp.com/")
+        ->header('Access-Control-Allow-Origin', "*")
         ->header('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE, OPTIONS")
         ->header('Access-Control-Allow-Headers', "Accept, Content-Type, Authorization");
     }
