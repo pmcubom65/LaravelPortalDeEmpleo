6e2a2f9c9e487477db7c041d04d71565e318f68c
pmcubom65 <pmcubom@zion.es>
2020-08-26 18:25:48 +0200

    cloudinary

--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,8 +1,8 @@
     var Ziggy = {
         namedRoutes: {"inicio":{"uri":"\/","methods":["POST"],"domain":null},"login":{"uri":"login","methods":["GET","HEAD"],"domain":null},"logout":{"uri":"logout","methods":["POST"],"domain":null},"register":{"uri":"register","methods":["GET","HEAD"],"domain":null},"password.request":{"uri":"password\/reset","methods":["GET","HEAD"],"domain":null},"password.email":{"uri":"password\/email","methods":["POST"],"domain":null},"password.reset":{"uri":"password\/reset\/{token}","methods":["GET","HEAD"],"domain":null},"password.update":{"uri":"password\/reset","methods":["POST"],"domain":null},"password.confirm":{"uri":"password\/confirm","methods":["GET","HEAD"],"domain":null},"apuntarse":{"uri":"search\/{id}","methods":["POST"],"domain":null},"editar":{"uri":"search\/{id}\/edit","methods":["POST"],"domain":null},"home":{"uri":"home","methods":["POST"],"domain":null},"Expe":{"uri":"home\/Expe\/{id}","methods":["POST"],"domain":null},"empresa":{"uri":"empresa\/{id}","methods":["POST"],"domain":null},"oferta":{"uri":"empresa\/{id}\/oferta","methods":["PUT","PATCH"],"domain":null},"contacto":{"uri":"empresa\/{id}\/published\/{ofertaid}\/trabajador\/{tid}","methods":["POST"],"domain":null},"publicada":{"uri":"empresa\/{id}\/published","methods":["GET","HEAD"],"domain":null},"candidatos":{"uri":"empresa\/{id}\/published\/{ofertaid}","methods":["PUT"],"domain":null},"cv":{"uri":"cv","methods":["GET","HEAD"],"domain":null}},
-        baseUrl: 'http://localhost/',
+        baseUrl: 'http://pedro-manuel-cubo-laravelvue.herokuapp.com/',
         baseProtocol: 'http',
-        baseDomain: 'localhost',
+        baseDomain: 'pedro-manuel-cubo-laravelvue.herokuapp.com',
         basePort: false,
         defaultParameters: []
     };
