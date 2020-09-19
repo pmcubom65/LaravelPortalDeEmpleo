3a5582bd9ca66d6c9f992e1ec8287aaeabac2094
pmcubom65 <pmcubom@zion.es>
2020-09-19 16:57:50 +0200

    cloudinary

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except_urls = [
-        'home/*'
+        'home/*',
+        'http://res.cloudinary.com/hoif30pep/*'
 
     ];
 
